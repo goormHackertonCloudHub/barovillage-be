93684ea9d985143f7dc608df4802280d7d53deab
minnie <radic7700@gmail.com>
2024-12-14 17:09:23 +0900

    feat: User 도메인 엔티티 개발

---
+++ src/main/java/com/cloudhub/baroVillage/domain/user/UserLocationRepository.java
@@ -0,0 +1,6 @@
+package com.cloudhub.baroVillage.domain.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserLocationRepository extends JpaRepository<UserLocation, Long> {
+}
