6b34a30941d4ecbf1920d8f2205f76392ffb49a3
minnie <radic7700@gmail.com>
2024-12-14 14:31:07 +0900

    initial commit

---
+++ src/main/java/com/cloudhub/baroVillage/BaroVillageApplication.java
@@ -0,0 +1,13 @@
+package com.cloudhub.baroVillage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaroVillageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaroVillageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloudhub/baroVillage/BaroVillageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudhub.baroVillage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaroVillageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
